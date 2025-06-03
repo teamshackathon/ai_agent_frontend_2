1a625def9439dd9e8a88e9aabc37196d7be66642
yonpachi <51481568+yonpachi@users.noreply.github.com>
2025-06-03 09:04:39 +0900

    add : useEvent.ts

--- src/lib/hook/useEvent.ts
+++ src/lib/hook/useEvent.ts
@@ -1,1 +1,18 @@
 // lib/hook/useEvent.ts
+
+import { useRef, useEffect, useCallback } from "react";
+
+export function useEvent<T extends (...args: any[]) => void>(handler: T): T {
+  const handlerRef = useRef(handler);
+
+  useEffect(() => {
+    handlerRef.current = handler;
+  }, [handler]);
+
+  return useCallback(
+    ((...args) => {
+      return handlerRef.current(...args);
+    }) as T,
+    []
+  );
+}
