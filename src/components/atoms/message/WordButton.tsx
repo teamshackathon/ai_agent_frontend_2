6559a5e2f4ded82a2688f4daab052a0bbd1c7721
yonpachi <51481568+yonpachi@users.noreply.github.com>
2025-06-04 15:08:42 +0900

    change : WordButton.tsx, ChatInput.tsx

--- src/components/atoms/message/WordButton.tsx
+++ src/components/atoms/message/WordButton.tsx
@@ -3,31 +3,37 @@
 import { Button, Wrap, WrapItem } from "@chakra-ui/react";
 
 type WordProps = {
-	word: string;
-	onSelect: (value: string) => void;
+  word: string;
+  onSelect: (value: string) => void;
+  disable?: boolean;
 };
 
 type ListProps = {
-	words: string[];
-	onSelect: (value: string) => void;
+  words: string[];
+  onSelect: (value: string) => void;
+  disable?: boolean;
 };
 
-export function WordButton({ word, onSelect }: WordProps) {
-	return (
-		<Button size="sm" onClick={() => onSelect(word)}>
-			{word}
-		</Button>
-	);
+export function WordButton({ word, onSelect, disable }: WordProps) {
+  return (
+    <Button size="sm" onClick={() => onSelect(word)} isDisabled={disable}>
+      {word}
+    </Button>
+  );
 }
 
-export default function WordButtonList({ words, onSelect }: ListProps) {
-	return (
-		<Wrap>
-			{words.map((word) => (
-				<WrapItem key={word}>
-					<WordButton word={word} onSelect={onSelect} />
-				</WrapItem>
-			))}
-		</Wrap>
-	);
+export default function WordButtonList({
+  words,
+  onSelect,
+  disable,
+}: ListProps) {
+  return (
+    <Wrap>
+      {words.map((word) => (
+        <WrapItem key={word}>
+          <WordButton word={word} onSelect={onSelect} disable={disable} />
+        </WrapItem>
+      ))}
+    </Wrap>
+  );
 }
