b747c5626d055bfacbc3c046205edd5b2895caed
yonpachi <51481568+yonpachi@users.noreply.github.com>
2025-06-04 14:47:12 +0900

    format

--- src/components/atoms/message/WordButton.tsx
+++ src/components/atoms/message/WordButton.tsx
@@ -3,31 +3,31 @@
 import { Button, Wrap, WrapItem } from "@chakra-ui/react";
 
 type WordProps = {
-  word: string;
-  onSelect: (value: string) => void;
+	word: string;
+	onSelect: (value: string) => void;
 };
 
 type ListProps = {
-  words: string[];
-  onSelect: (value: string) => void;
+	words: string[];
+	onSelect: (value: string) => void;
 };
 
 export function WordButton({ word, onSelect }: WordProps) {
-  return (
-    <Button size="sm" onClick={() => onSelect(word)}>
-      {word}
-    </Button>
-  );
+	return (
+		<Button size="sm" onClick={() => onSelect(word)}>
+			{word}
+		</Button>
+	);
 }
 
 export default function WordButtonList({ words, onSelect }: ListProps) {
-  return (
-    <Wrap>
-      {words.map((word, idx) => (
-        <WrapItem key={idx}>
-          <WordButton word={word} onSelect={onSelect} />
-        </WrapItem>
-      ))}
-    </Wrap>
-  );
+	return (
+		<Wrap>
+			{words.map((word) => (
+				<WrapItem key={word}>
+					<WordButton word={word} onSelect={onSelect} />
+				</WrapItem>
+			))}
+		</Wrap>
+	);
 }
